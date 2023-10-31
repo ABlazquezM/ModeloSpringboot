d53a74e8185471adda55ef9d00edf47ea57e17ee
ABlazquezM <andrea.blazquez.martin@gmail.com>
2023-10-31 20:06:02 +0100

    Creación de Proyecto Modelo SpringBoot

---
+++ src/main/java/spring/modelos/ModeloSpringbootApplication.java
@@ -0,0 +1,13 @@
+package spring.modelos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/modelos/ModeloSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.modelos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
